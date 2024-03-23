13fb24ab7f86cff047ab91828631df67a8b409be
themuffinator <djdacosta86@yahoo.co.uk>
2024-03-23 23:15:09 +0000

    Version to 0.12.6

--- src/g_local.h
+++ src/g_local.h
@@ -10,7 +10,7 @@
 constexpr const char *GAMEVERSION = "baseq2";
 
 constexpr const char *GAMEMOD_TITLE = "Muff Mode BETA";
-constexpr const char *GAMEMOD_VERSION = "0.12.5";
+constexpr const char *GAMEMOD_VERSION = "0.12.6";
 
 //==================================================================
 
