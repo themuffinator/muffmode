926e478f134bac6df746a1f33a73d2e75dc5bc80
themuffinator <djdacosta86@yahoo.co.uk>
2025-10-03 11:13:36 +0100

    Ensure exported API symbols use C linkage

--- src/game.h
+++ src/game.h
@@ -104,7 +104,7 @@ constexpr bit_t<n> bit_v = 1ull << n;
 #elif defined(KEX_Q2GAME_IMPORTS)
 #define Q2GAME_API extern "C" __declspec( dllimport )
 #else
-#define Q2GAME_API
+#define Q2GAME_API extern "C"
 #endif
 
 // game.h -- game dll information visible to server
